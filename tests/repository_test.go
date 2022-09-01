b6d745477973dfb9f892d60d073803cebda6ce66
Raúl García <rgarciaxxx@yahoo.com.mx>
2022-09-01 17:52:15 +0000

    Incluye datos reales en prueba de procesamiento de alarmas

--- tests/repository_test.go
+++ tests/repository_test.go
@@ -536,22 +536,25 @@ func TestRepo_ProcesaAlarmasLista(t *testing.T) {
 		t.Fatal("No se conectó")
 	}
 
-	v := int64(0)
+	v0 := int64(0)
+	v3 := int64(3)
+	v16 := int64(16)
+	v1005 := int64(1005)
 
 	alarmas := models.ListaAlarmasParam{
-		Id:               1,
-		MsgIds:           []int64{1234567, 7654321},
-		MsgSlots:         []int32{7, 22},
-		MsgPorts:         []*int64{&v, nil},
-		MsgTexts:         []string{"No Connection PRUEBA1", "Ouput bitrate PRUEBA2"},
-		MsgSourcesNames:  []string{"Stream", "Demodulation"},
-		MsgSeveryties:    []string{"CRITICAL", "MAJOR"},
-		MsgInstances:     []int32{3, 0},
-		MsgSetTimes:      []string{"2022-08-19 14:11:02+00", "2022-08-19 14:11:15"},
-		MsgCardIds:       []int32{73, 17},
-		TimestampInicios: []string{"2022-08-21 21:26:21+00", "2022-08-21 21:26:21+00"},
-		DateServer:       "2022-08-19",
-		TimeServer:       "19:17:22",
+		Id:               11,
+		MsgIds:           []int64{4390932, 1048669, 3473417, 1048589, 1048589, 1048580, 3473418, 1048611, 4259950, 3539048, 3539045, 2883585, 1048580},
+		MsgSlots:         []int32{1, 5, 8, 14, 0, 0, 8, 9, 16, 16, 16, 1, 5},
+		MsgPorts:         []*int64{&v3, nil, nil, nil, nil, &v1005, nil, nil, &v0, &v0, &v16, &v0, &v0},
+		MsgTexts:         []string{"No Lock", "Output bitrate exceeded on 230.2.3.6:6000", "Bulkdescrambler not authorised. All services blocked", "No signal", "No signal on port A", "239.255.1.4:1234: No bitrate on: Service 13005", "Latens controller connectivity failure", "Link down input", "Control port link down", "No Connection from IP 10.101.1.1, port 6002", "Control port link down", "PMT Missing: Service 13077: Service 13077 (From Hotbird Vert. Low)", "239.200.1.1:1234: No bitrate on: Service 13005"},
+		MsgSourcesNames:  []string{"Demodulation", "Stream", "bulkdscr-latens", "Stream", "Stream", "Stream", "bulkdscr-latens", "Network", "Network", "EMMG/PDG", "Network", "PSI", "Stream"},
+		MsgSeveryties:    []string{"CRITICAL", "CRITICAL", "CRITICAL", "CRITICAL", "CRITICAL", "CRITICAL", "MAJOR", "CRITICAL", "CRITICAL", "CRITICAL", "CRITICAL", "CRITICAL", "CRITICAL"},
+		MsgInstances:     []int32{0, 11, 0, 0, 0, 0, 0, 0, 0, 3, 0, 13077, 0},
+		MsgSetTimes:      []string{"2021-09-16 16:30:47", "2022-08-22 11:40:02", "2021-02-10 14:21:30", "2021-06-08 16:33:24", "2022-04-03 06:33:42", "2022-08-21 09:25:15", "2021-02-10 14:21:30", "2022-02-17 17:24:33", "2022-07-08 20:45:30", "2022-07-08 20:48:35", "2022-07-08 20:48:05", "2022-08-21 09:25:14", "2022-08-21 09:25:14"},
+		MsgCardIds:       []int32{16, 13, 51, 14, 32, 37, 51, 90, 11, 11, 11, 16, 13},
+		TimestampInicios: []string{"2020-10-01 20:56:52", "2022-08-12 11:15:22", "2019-07-23 16:38:18", "2020-03-15 21:02:06", "2021-11-03 01:02:42", "2022-08-10 06:45:48", "2019-07-23 16:38:18", "2021-08-05 22:44:24", "2022-05-15 05:26:18", "2022-05-15 05:32:28", "2022-05-15 05:31:28", "2022-08-10 06:45:46", "2022-08-10 06:45:46"},
+		DateServer:       "2022-09-01",
+		TimeServer:       "12:04:42",
 	}
 
 	repo := database.NewAteneaRepo(cn)
